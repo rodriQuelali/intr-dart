e1169773c4093886a6728e699c4f0d8f15bfd0a7
Rodri Quelali <rodriquelali@gmail.com>
2024-03-15 00:06:35 -0400

    tipo de datos, listas

---
+++ introduccion.dart
@@ -0,0 +1,6 @@
+void main() {
+  //var -- > para crear variables
+  //String -- tipo de datos objetos
+  String nombre = "rodrigo";
+  print("holaaaa $nombre");
+}
---
+++ tipoLista.dart
@@ -0,0 +1,21 @@
+void main() {
+
+  // lista tambien se lo llama array o vectores
+
+  //lista dynamic, o dinamico
+  List numeros = [1,2,3,4,5,6];
+  print(numeros);
+
+  //adicionando datos a la lista
+  numeros.add("helolo word!!");
+  print(numeros);
+
+  //lista tipada
+  List <int> datos = [1,2,5];
+  
+
+  //lista de tamaño fijo
+  List maasNumeros = List(10);
+  print(maasNumeros);
+
+}
